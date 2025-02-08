5ddcaa0903129ac7df3b1998ffe0324c1cad5787
diyajjain <diya.jain@duke.edu>
2025-02-08 16:09:20 -0500

    front page created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,21 +3,36 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+    <div className="container">
+
+    
+      <div className="content">
+    
+      <header className="header">
+        <div className="logo">
+          <img src="houselogo.png" alt="Sheltr Logo" className="logo-img" />
+          <h1 className="brand-name">sheltr</h1>
+        </div>
       </header>
+
+      <section className = "hero">
+        <h2 className="hero-title">A Safe Place <br /> After Disaster</h2>
+        <p className="hero-subtitle">
+          Helping disaster survivors find temporary homes.
+        </p>
+      </section>
+
+      <section className = "signup">
+        <button className = "findOrOffer">Find a Home</button>
+        <button className = "findOrOffer">Offer a Home</button>
+      </section>
+    </div>
+
+    <div className="image">
+        {/* The background image will fill this section */}
+    </div>
+
     </div>
   );
 }
